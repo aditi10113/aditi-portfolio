fc99f81e14231f31b80d31ad126a443268515832
Aditi bhargava <132725294+aditi10113@users.noreply.github.com>
2024-07-07 08:36:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add any JavaScript you need for interactivity
+// Example: Form submission handler
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
+
+
+
+
+
